61836530f58e2f26c16db620e9a4165a40ec16bb
Tze-Ping Low <lowspin@gmail.com>
2018-05-04 23:41:32 -0500

    read preset drive commands from custom csv file

--- src/blindmove/src/blindmove.py
+++ src/blindmove/src/blindmove.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python
 
+import os
+import csv
 import time
+
 import rospy
 from geometry_msgs.msg import Twist
+
+CSV_HEADER = ['throttle', 'steer', 'duration']
+
+class DriveCmd:
+    def __init__(self, throttle, steer, duration):
+        self.throttle = throttle
+        self.steer = steer
+        self.duration = duration
 
 class BlindDrive(object):
     def __init__(self):
@@ -16,27 +27,46 @@ class BlindDrive(object):
         self.max_linear_vel = rospy.get_param('~max_linear_vel',0.2)
         self.max_angular_vel = rospy.get_param('~max_angular_vel',1.5707)
 
-        #print 'max linear vel = ' + str(self.max_linear_vel) + ', max angular vel = ' + str(self.max_angular_vel)
         self.driveroutine()
 
     def driveroutine(self):
-        self.drive(1.0, 0.0, 2.0)
-        self.drive(-1.0, 1.0, 3.8)
-        self.drive(-1.0, 0.5, 0.7) 
+        drivecommands = self.new_command_loader(rospy.get_param('~divecmdfile'))
+        for cmd in drivecommands:
+            self.drive(cmd.throttle, cmd.steer, cmd.duration)
+        print 'All Commands Executed. Ctrl-c to end'
 
-    def drive(self, cmdthrottle, cmdsteer, cmdduration):
-        
-        print 'throttle ' + str(cmdthrottle) + ', steer ' + str(cmdsteer) + ', duration ' + str(cmdduration)
+    def new_command_loader(self, path):
+        if os.path.isfile(path):
+            drivecommands = self.load_commands(path)
+            rospy.loginfo('Drive Commands Loaded')
+            return drivecommands
+        else:
+            rospy.logerr('%s is not a file', path)
+
+    def load_commands(self, fname):
+        commands = []
+        with open(fname) as wfile:
+            reader = csv.DictReader(wfile, CSV_HEADER)
+            for cmd in reader:
+                throttle = float(cmd['throttle'])
+                steer = float(cmd['steer'])
+                duration = float(cmd['duration'])
+                print 'Load cmd = ' + str(throttle) + ', ' + str(steer) + ', ' + str(duration)
+                c = DriveCmd(throttle,steer,duration)
+                commands.append(c)
+        return commands
+
+    def drive(self, cmdthrottle, cmdsteer, cmdduration):        
+        print 'Execute: throttle ' + str(cmdthrottle) + ', steer ' + str(cmdsteer) + ', duration ' + str(cmdduration)
         self.twist.linear.x = self.max_linear_vel * cmdthrottle
         self.twist.angular.z = self.max_angular_vel * cmdsteer
-        print 'twist.linear=' + str(self.twist.linear.x) + ', twist.angular=' + str(self.twist.angular.z)
+        #print 'twist.linear=' + str(self.twist.linear.x) + ', twist.angular=' + str(self.twist.angular.z)
 
         dt = 0.02
         rate = rospy.Rate(1/dt)
         numsteps = int(cmdduration / dt);
         for tt in range(numsteps):
             self.pub.publish(self.twist)
-            #time.sleep(dt)
             rate.sleep()
 
         #self.pub.publish(self.twist)
